25f9a3e75f20ce1d4cd0d7feee57984a094854a3
Rafael Simas <rafael.simas@al.resilia.com.br>
2021-12-22 15:49:49 -0300

    Finalizado requisito de seleção de imagem

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
-const nomeUsuario = prompt('Olá bem vindo ao FahsionDriven. Qual é o seu nome ?')
- 
+/* const nomeUsuario = prompt('Olá bem vindo ao FahsionDriven. Qual é o seu nome ?')
+  */
+function getImage(selected){
+  const option = document.querySelector(selected)
+  option.classList.add('selected')
+  console.log(option)
+}
