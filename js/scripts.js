60382c5e19cd2d42730c696b8138fc1c7d5c0c75
Rafael Simas <rafael.simas@al.resilia.com.br>
2021-12-23 11:46:23 -0300

    Adicionadas as funcionalidades refatoradas da lógica

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+const nome = prompt('Olá seja muito bem vindo ao FashionDriven. Qual é o seu nome ?')
+
+let contador = 0
+
+let modelo;
+let gola;
+let tecido;
+
+
+/* ====================PRATOS======================================= */
+function selecionarModelo(seletor, nome){
+    modelo = nome
+    const selecionado = document.querySelector('.modelo .selecionado')
+
+    if(selecionado !== null){
+        selecionado.classList.remove('selecionado')
+    }else {
+
+        contador += 1
+    }
+
+    const opcao = document.querySelector(seletor)
+    opcao.classList.add('selecionado')
+
+    habilitarBotaoSePedidoCompoleto()
+}
+
+/* ====================BEBIDAS======================================= */
+function selecionarGola(seletor, nome){
+    gola = nome
+    const selecionado = document.querySelector('.gola .selecionado')
+
+    if(selecionado !== null){
+        selecionado.classList.remove('selecionado')
+    }else {
+        contador += 1
+    }
+
+    const opcao = document.querySelector(seletor)
+    opcao.classList.add('selecionado')
+
+    habilitarBotaoSePedidoCompoleto()
+}
+/* ====================SOBREMESA======================================= */
+function selecionarTecido(seletor, nome){
+    tecido = nome
+    const selecionado = document.querySelector('.tecido .selecionado')
+
+    if(selecionado !== null){
+        selecionado.classList.remove('selecionado')
+    }else {
+        contador += 1
+    }
+
+    const opcao = document.querySelector(seletor)
+    opcao.classList.add('selecionado')
+
+    habilitarBotaoSePedidoCompoleto()
+}
+
+function habilitarBotaoSePedidoCompoleto(){
+   
+   if(contador === 3){
+    const botao = document.querySelector('.fazer-pedido')
+    botao.classList.add('ativo')
+    botao.innerHTML = 'Fechar pedido'
+    const precoTotal =  precoPrato + precoBebida + precoSobremesa
+    const mensagem = "Olá eu gostaria de fazer o pedido:\n- Prato: " + prato + "\n Bebida: " + bebida + "\n Sobremesa: " + doce +"\n Total: " + precoTotal.toFixed(2) + " "
+    const mensagemFormatada = encodeURIComponent(mensagem) 
+    const url = 'https://wa.me/5521996119461?text=' + mensagemFormatada;
+    botao.setAttribute('href', url)
+   }
+}
