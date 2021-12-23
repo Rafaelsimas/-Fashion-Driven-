51bf56a6608364114b9807191c1fd41072a56c87
Rafael Simas <rafael.simas@al.resilia.com.br>
2021-12-23 11:45:14 -0300

    Adicionadas as funcionalidades refatoradas da lógica

--- js/script.js
+++ js/script.js
@@ -1,7 +1,71 @@
-/* const nomeUsuario = prompt('Olá bem vindo ao FahsionDriven. Qual é o seu nome ?')
-  */
-function getImage(selected){
-  const option = document.querySelector(selected)
-  option.classList.add('selected')
-  console.log(option)
+let contador = 0
+
+let modelo;
+let gola;
+let tecido;
+
+
+/* ====================MODELO======================================= */
+function selecionarModelo(seletor, nome){
+    modelo = nome
+    const selecionado = document.querySelector('.modelo .selecionado')
+
+    if(selecionado !== null){
+        selecionado.classList.remove('selecionado')
+    }else {
+
+        contador += 1
+    }
+
+    const opcao = document.querySelector(seletor)
+    opcao.classList.add('selecionado')
+
+    habilitarBotaoSePedidoCompoleto()
+}
+
+/* ====================GOLA======================================= */
+function selecionarGola(seletor, nome){
+    gola = nome
+    const selecionado = document.querySelector('.gola .selecionado')
+
+    if(selecionado !== null){
+        selecionado.classList.remove('selecionado')
+    }else {
+        contador += 1
+    }
+
+    const opcao = document.querySelector(seletor)
+    opcao.classList.add('selecionado')
+
+    habilitarBotaoSePedidoCompoleto()
+}
+/* ====================TECIDO======================================= */
+function selecionarTecido(seletor, nome){
+    tecido = nome
+    const selecionado = document.querySelector('.tecido .selecionado')
+
+    if(selecionado !== null){
+        selecionado.classList.remove('selecionado')
+    }else {
+        contador += 1
+    }
+
+    const opcao = document.querySelector(seletor)
+    opcao.classList.add('selecionado')
+
+    habilitarBotaoSePedidoCompoleto()
+}
+
+function habilitarBotaoSePedidoCompoleto(){
+   
+   if(contador === 3){
+    const botao = document.querySelector('.fazer-pedido')
+    botao.classList.add('ativo')
+    botao.innerHTML = 'Fechar pedido'
+    const precoTotal =  precoPrato + precoBebida + precoSobremesa
+    const mensagem = "Olá eu gostaria de fazer o pedido:\n- Prato: " + prato + "\n Bebida: " + bebida + "\n Sobremesa: " + doce +"\n Total: " + precoTotal.toFixed(2) + " "
+    const mensagemFormatada = encodeURIComponent(mensagem) 
+    const url = 'https://wa.me/5521996119461?text=' + mensagemFormatada;
+    botao.setAttribute('href', url)
+   }
 }
